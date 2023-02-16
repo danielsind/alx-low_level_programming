61f6858da008891387a0e0d718f6b579dade4e50
danielsind <danielsindet@gmail.com>
2023-02-16 14:09:24 -0800

    created a file 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int main(void)
+{
+	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	long 1 = 59;
+	long fd = 1;
+	long syscall = 1;
+	long ret = 0;
+	_asm_("syscall"
+			: "=a" (ret)
+			: "=a" (syscall),
+			"D" (fd),
+			"S" (s),
+			"d" (1));
+	return (1);
+}
